065672470a8d8c42d8ea11a1e76896c2766e2d49
Iñaki <inaki.lastra@gmail.com>
2024-08-07 12:31:34 +0200

    ex03

---
+++ ex03/load_image.py
@@ -0,0 +1,77 @@
+import os
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Carga una imagen desde el archivo especificado y devuelve su contenido de
+    píxeles en formato RGB.
+
+    Args:
+        path (str): La ruta del archivo de imagen.
+
+    Returns:
+        np.ndarray: Un array numpy que contiene los valores de los píxeles en
+        formato RGB.
+    """
+    try:
+        if not path.lower().endswith(("jpg", "jpeg")):
+            print("\033[38;5;209mOnly JPG and JPEG formats are supported."
+                  "\033[0;39m")
+            return ""
+        if not os.path.exists(path):
+            print("\033[38;5;209mFile not found:", path, "\033[0;39m")
+            return ""
+        image = Image.open(path)
+        image = image.convert('RGB')
+        image_array = np.array(image)
+        print("\033[0;92mThe shape of image is: ",image_array.shape,
+              "\033[0;39m")
+        print("\033[0;92m",image_array,"\033[0;39m")
+        return image_array
+    except Exception as error:
+        print(f"\033[38;5;209mError: {error}\033[0;39m")
+        return ""
+    
+
+'''
+import numpy as np
+from PIL import Image
+import os
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Load an image from the specified path and return it as a NumPy array.
+
+    Parameters:
+    path (str): The path to the image file to be loaded.
+
+    Returns:
+    np.ndarray: A NumPy array representing the loaded image.
+
+    Raises:
+    AssertionError: If the image format is wrong or the file is not found.
+
+    Loads an image from the given path and performs checks to ensure
+    compatibility and validity. Only JPG and JPEG formats are supported. If the
+    file does not exist or the format is not supported, an Error is raised.
+    The image is then converted to a NumPy array and its shape is printed.
+
+    """
+    try:
+        if not path.lower().endswith(("jpg", "jpeg")):
+            raise AssertionError("Only JPG and JPEG formats are supported.")
+        if not os.path.exists(path):
+            raise AssertionError("File not found:", path)
+        img = Image.open(path)
+        print(
+            f"The shape of Image is: {img.size[1]},{img.size[0]}, {img.layers}"
+            )
+        return np.array(img)
+    except AssertionError as error:
+        print("\033[31m", AssertionError.__name__ + ":", error, "\033[0m")
+        return ""
+    
+'''
---
+++ ex03/zoom.py
@@ -0,0 +1,156 @@
+from load_image import ft_load
+import matplotlib.pyplot as plt
+import numpy as np
+
+def zoom_on_image(image: np.ndarray, zoom_factor: float) -> np.ndarray:
+    """
+    Realiza un zoom en el centro de la imagen.
+
+    Args:
+        image (np.ndarray): La imagen original.
+        zoom_factor (float): El factor de zoom.
+
+    Returns:
+        np.ndarray: La imagen después del zoom.
+    """
+    # Obtener las dimensiones de la imagen
+    height, width, _ = image.shape
+
+    # Calcular las nuevas dimensiones
+    new_height = int(height / zoom_factor)
+    new_width = int(width / zoom_factor)
+
+    # Calcular los bordes para el recorte
+    top = (height - new_height) // 2
+    left = (width - new_width) // 2
+    bottom = top + new_height
+    right = left + new_width
+
+    # Recortar la imagen
+    zoomed_image = image[top:bottom, left:right]
+
+    return zoomed_image
+
+def main():
+    # Cargar la imagen
+    image = ft_load("animal.jpeg")
+
+    if image is not None:       
+        # Realizar el zoom en la imagen
+        zoom_factor = 2  # Cambia esto para ajustar el nivel de zoom
+        zoomed_image = zoom_on_image(image, zoom_factor)
+        
+        # Imprimir la nueva forma de la imagen
+        print("\033[0;92mNew shape after slicing: ",zoomed_image.shape, "\033[0;39m")
+        print("\033[0;92m",zoomed_image,"\033[0;39m")
+        # Mostrar la imagen original y la imagen con zoom
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        axes[0].imshow(image)
+        axes[0].set_title("Original Image")
+        axes[1].imshow(zoomed_image)
+        axes[1].set_title("Zoomed Image")
+        plt.show()
+    else:
+        print("Failed to load image.")
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+from load_image import ft_load
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import sys
+import os
+
+
+def print_rows_firstelem(arr, int):
+    """
+    Print a formatted display of the first elements
+    in each row of a given array.
+
+    Parameters:
+    arr (array-like): The input array containing elements to be displayed.
+    int (int): An integer specifying the display format: 0 for single brackets,
+               1 for triple brackets.
+
+    Iterates through the rows of the input array and displays the first
+    elements of each row in a formatted manner. The display format is
+    determined by the 'int' parameter. For 'int' equal to 0, single
+    brackets are used to enclose the elements, while for 'int' equal
+    to 1, triple brackets are used.
+    """
+    count = 0
+    for row in arr:
+        count += 1
+    length = count
+    count = 0
+    for row in arr:
+        if count == 0:
+            if int == 1:
+                print("[[[", row[0], "]", sep="")
+            else:
+                print("[[", row[0], sep="")
+        if count > 0 and count < 3 or count > length - 4:
+            if int == 1:
+                if count == length - 1:
+                    print("  [", row[0], "]]]", sep="")
+                elif count < length - 1:
+                    print("  [", row[0], "]", sep="")
+            else:
+                if count == length - 1:
+                    print("  ", row[0], "]]", sep="")
+                else:
+                    print("  ", row[0], sep="")
+        if count == 2:
+            print("  ...")
+        count += 1
+
+
+def main():
+    """
+    Load, process, and display an image based on command-line arguments.
+
+    This function serves as the main entry point of the script. It loads an
+    image from the command-line argument, performs various image processing
+    operations, and displays the resulting images. The script supports
+    cropping, grayscale conversion, and zoomed image display. Errors related
+    to file format and existence are caught and displayed.
+    """
+    try:
+        path = sys.argv[1]
+        if not path.lower().endswith(("jpg", "jpeg")):
+            raise AssertionError("Only JPG and JPEG formats are supported.")
+        if not os.path.exists(path):
+            raise AssertionError("File not found:", path)
+        image = Image.open(path)
+        if image is None:
+            raise AssertionError("Failed to load image.")
+
+        print_rows_firstelem(ft_load(path), 0)
+        image.show()
+
+        zoomed_image = image.crop((400, 100, 800, 600))
+        zoomed_image.save("zoomed_image.jpg")
+        print(f"New shape after cropping: {zoomed_image.size}")
+
+        grayscale_image = zoomed_image.convert("L")
+        grayscale_image.show()
+        print_rows_firstelem(np.array(grayscale_image), 1)
+
+        plt.imshow(zoomed_image)
+        plt.title("Zoomed Image")
+        plt.axis('on')
+        plt.show()
+
+    except AssertionError as error:
+        print(AssertionError.__name__ + ":", error)
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''
